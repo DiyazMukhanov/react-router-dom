4561cb1b583f41ec48c4caad7b42cb4a680c3dc9
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-10-28 13:05:49 +0400

    Basic functionalities part 2

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -9,6 +9,7 @@ import Root, { loader as rootLoader, action as rootAction, } from "./routes/root
 import ErrorPage from "./error-page";
 import Contact, {
   loader as contactLoader,
+  action as contactAction,
 } from "./routes/contact";
 import EditContact, {
   action as editAction,
@@ -24,12 +25,16 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
-      { index: true, element: <Index /> },
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
       {
         path: "contacts/:contactId/edit",
         element: <EditContact />,
@@ -42,7 +47,7 @@ const router = createBrowserRouter([
         errorElement: <div>Oops! There was an error.</div>,
       },
     ],
-  }
+  }]}
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
